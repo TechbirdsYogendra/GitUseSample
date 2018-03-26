5ed1e9aa4ea65be4894d1981fd2bca4081daec1d
Yogendra <yogendra@veimac160915.local>
2018-03-26 12:12:16 +0530

    Added map Class

---
+++ GitUseSample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseSample
+//
+//  Created by Yogendra on 3/26/18.
+//  Copyright © 2018 Yogendra. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
